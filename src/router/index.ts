52ef913ae34767ae0c8867c830b64a5c95479a71
123ang <angjinsheng@gmail.com>
2023-09-22 09:19:51 +0800

    update login and register

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
   },
   {
     path: '/about',
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,11 @@
+<template>
+    Register
+    name
+    email
+    password
+    confirm password
+    gender
+    dob
+    phone
+    <input type="checkbox"> Agree to term and condition
+</template>
