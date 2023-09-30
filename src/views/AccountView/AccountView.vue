1692fb945750bb928f03795ba66404a618c61333
123ang <angjinsheng@gmail.com>
2023-09-30 21:24:27 +0800

    Update AccountView.vue

--- src/views/AccountView/AccountView.vue
+++ src/views/AccountView/AccountView.vue
@@ -1,55 +1,91 @@
 <template>
   <div class="body-section">
     <div class="container">
-      <div class="my-5"><h2>My Account</h2></div>
-      <hr>
-      <div class="row">
-        
-        <div class="col-md-12">
-          <h4>First Name:</h4>
-          <input type="text" name="first_name" id="first_name" placeholder="First Name" value="John">
-        </div>        
-        <div class="col-md-12">
-          <h4>Last Name:</h4>
-          <input type="text" name="last_name" id="last_name" placeholder="Last Name" value="Doe">
-        </div>        
-        <div class="col-md-12">
-          <h4>Mobile Number:</h4>
-          <input type="tel" name="phone" id="phone" placeholder="Mobile Number" value="012-3456789">
-        </div> 
-        <div class="col-md-12">
-          <h4>Email:</h4>
-          <p>mail@mail.com</p>
-        </div>     
-        <div class="col-md-12">
-          <h4>Gender:</h4>
-          <p>Male</p>
-        </div>        
-        <div class="col-md-12">
-          <h4>Date of Birth:</h4>
-          <p>01 / 01 / 2000</p>
-        </div>                
+      <div v-if="userProfile">
+        <div class="my-5">
+          <h2>My Account</h2>
+        </div>
+        <hr>
+        <div class="row">
 
+          
+          <div class="col-md-12">
+            <h4>Name:</h4>
+            <input type="text" name="name" id="name" placeholder=" Name" :value="userProfile.name">
+          </div>
+
+          <div class="col-md-12">
+            <h4>Mobile Number:</h4>
+            <input type="tel" name="phone" id="phone" placeholder="Mobile Number" :value="userProfile.phone">
+          </div>
+
+          <div class="col-md-12">
+            <h4>Email:</h4>
+            <p>{{ userProfile.email }}</p>
+          </div>
+
+          <div class="col-md-12">
+            <h4>Gender:</h4>
+            <p>{{ userProfile.gender }}</p>
+          </div>
+
+          <div class="col-md-12">
+            <h4>Date of Birth:</h4>
+            <p>{{ userProfile.dob }}</p>
+          </div>
+
+        </div>
+
+        <div class="my-5 button-container">
+          <button>Save Changes</button>
+        </div>
       </div>
-      
-      <div class="my-5 button-container">
-        <button>Save Changes</button>
-      </div>             
 
+      <div v-else>
+        <p>Loading profile...</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useStore } from 'vuex';
+
+interface UserProfile {
+  name: string;
+  phone: string;
+  email: string;
+  gender: string;
+  dob: string; // Adjust if the returned format is different
+  // Add other fields if needed
+}
 
 export default defineComponent({
   name: 'AccountView',
-  props: {},
-  setup() {}
-});
+  setup() {
+    const store = useStore();
+    const userProfile = ref<UserProfile | null>(null);
+    
+    onMounted(async () => {
+      try {
+        const url = `${store.state.host_url}user/profile/${store.state.user_id}`;
+        console.log(url)
+        const response = await axios.post(url);
+        userProfile.value = response.data.user;
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      }
+    });
 
+    return {
+      userProfile
+    };
+  }
+});
 </script>
+
 
 <style lang="scss" scoped>
   @import '@/assets/styles/global.styles.scss';
