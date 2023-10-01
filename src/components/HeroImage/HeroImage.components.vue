fd3f4b7ce2e6f81982ccb3e089d72d898bce5a0c
123ang <angjinsheng@gmail.com>
2023-10-02 02:38:38 +0800

    update language

--- src/components/HeroImage/HeroImage.components.vue
+++ src/components/HeroImage/HeroImage.components.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="hero-section">
-        <div class="container">
-            <div class="title-container">
-                <h1 class="mb-2">MyFreeLancer</h1>
-                <h4 class="mb-2">MyFreeLancer is a dynamic platform connecting talented freelancers with businesses and individuals seeking specialized skills. </h4>
-                <div class="hero-button-container">
-                    <a href="#"><HeroButton :title="'GET A FREELANCER'" /></a>
-                    <a href="#"><HeroButton :title="'START TO EARN MONEY'" /></a>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="hero-section">
+      <div class="container">
+          <div class="title-container">
+              <h1 class="mb-2">{{ $t('message.MyFreeLancer') }}</h1>
+              <h4 class="mb-2">{{ $t('message.MyFreeLancer is a dynamic platform connecting talented freelancers with businesses and individuals seeking specialized skills') }}</h4>
+              <div class="hero-button-container">
+                  <a href="#"><HeroButton :title="$t('message.GET A FREELANCER')" /></a>
+                  <a href="#"><HeroButton :title="$t('message.START TO EARN MONEY')" /></a>
+              </div>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script lang="ts">
